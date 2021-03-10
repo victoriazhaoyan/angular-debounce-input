d0c1dedcc9b9b57619eff2367e862eebf9a6e6e3
victoriazhaoyan <victoriazhao123@gmail.com>
2021-03-10 15:54:51 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-eyb8hp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-eyb8hp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, VERSION } from "@angular/core";
+import { debounceTime } from "rxjs/internal/operators/debounceTime";
+import { distinctUntilChanged } from "rxjs/internal/operators/distinctUntilChanged";
+import { Subject } from "rxjs/internal/Subject";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  public consoleMessages: string[] = [];
+  public userQuestion: string;
+  userQuestionUpdate = new Subject<string>();
+
+  constructor() {
+    // Debounce search.
+    this.userQuestionUpdate
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(value => {
+        this.consoleMessages.push(value);
+      });
+  }
+}
